a97f7529363bb7777708b67f511460c74070fa48
sagiri <sagiri@ebay.com>
2018-07-08 11:12:13 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# TODO MVC Application using React & Redux
+
+Inspired by [TodoMVC](http://todomvc.com/)
+
+## Learning agenda
+    1. Setup a simple app
+    2. Hello World
+    3. Brush up ES6
---
+++ src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+    <div id="sampleElement">
+        <p> Hello World! </p>
+    </div>
+    , document.getElementById('root'));
